0475eedf98ade93bf8a1d91f11006adca0153e76
songmk96 <48798925+songmk96@users.noreply.github.com>
2022-02-04 14:19:08 +0900

    제어문을 사용하여 삽입하는 프로시저
    데이터를 삽입하기 전에 IF문으로 같은 도서가 있는지 점검한 후 삽입하는 프로시저
    
    <PL/SQL 제어문>
    BEGIN-END문 : 하나 이상의 문장이 수행될 수 있도록 블록을 정의
    IF-ELSE 문 : 조건을 검사하고 검사 결과에 따라 코드의 어느 부분이 사용되는지를 결정
    WHILE문 : 조건이 참인 동안 코드의 일정 부분을 반복
    RETURN문 : 코드 구조(저장 프로시저나 트리거)에 상관없이 블록을 빠져나와 PL/SQL문을 종료

---
+++ 예제 5-2.sql
@@ -0,0 +1,27 @@
+CREATE OR REPLACE PROCEDURE BookInsertOrUpadate(
+    myBookID NUMBER,
+    myBookName VARCHAR2,
+    myPublisher VARCHAR2,
+    myPrice INT)
+AS
+    mycount NUMBER;
+BEGIN
+    SELECT count(*) INTO mycount FROM Book
+        WHERE bookname LIKE myBookName;
+    IF mycount != 0 THEN
+        UPDATE Book SET price = myPrice
+            WHERE bookname LIKE myBookName;
+    ELSE
+        INSERT INTO Book(bookid, bookname, publisher, price)
+            VALUES(myBookID, myBookName, myPublisher, myPrice);
+    END IF;
+END;
+/
+----------------------------------------------------------------------------
+
+--BookInsertOrUpdate 프로시저를 실행하여 테스트하는 부분
+EXEC BookInsertOrUpadate(15, '스포츠 즐거움', '마당과학서적', 25000);
+SELECT * FROM BOOK;
+
+EXEC BookInsertOrUpadate(15, '스포츠 즐거움', '마당과학서적', 20000);
+SELECT * FROM BOOK;
