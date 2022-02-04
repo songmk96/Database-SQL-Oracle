3371ac6e7937fb7574a7fa965fe3c42a30149718
songmk96 <48798925+songmk96@users.noreply.github.com>
2022-02-04 09:34:11 +0900

    사용자 정의 함수 예제
    사용자 정의 함수(user defined function) : 수학의 함수와 마찬가지로 입력된 값을 가공하여 결과값을 되돌려준다.
    
    프로시저와 비슷해 보이지만 프로시저는 EXEC 명령에 따라 실행되는 독립된 프로그램이고, 사용자 정의 함수는 SELECT 문이나 프로시저 내에서 호출되어 SQL 문이나 프로시저에 그 값을 제공하는 용도로 사용된다.
    
    오라클에서 작성할 수 있는 사용자 정의 함수는 단일 값을 돌려주는 스칼라 함수가 일반적이다.

---
+++ 예제 5-6.sql
@@ -0,0 +1,16 @@
+CREATE OR REPLACE FUNCTION fnc_Interest(
+    Price NUMBER) RETURN INT
+IS
+    myInterest NUMBER;
+BEGIN
+/* 가격이 30,000원 이상이면 10%, 30,000원 미만이면 5% */
+    IF Price >= 30000 THEN myInterest := Price * 0.1;
+    ELSE myInterest := Price * 0.05;
+    END IF;
+    RETURN myInterest;
+END;
+/
+--------------------------------------------------------
+/* Orders 테이블에서 각 주문에 대한 이익을  출력 */
+SELECT custid, orderid, saleprice, fnc_Interest(saleprice) interest
+FROM Orders;
